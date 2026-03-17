d40f507a46f57f907f63e81f75fd92e327000335
Markman <davimarkmancastilho1@gmail.com>
2026-03-17 18:54:57 -0300

    dia 17/03, fim da fase 2

---
+++ ProgramaTreinamento/Fase2/React-Router-v6/tutorial-react-router-dom/src/components/Menu/index.tsx
@@ -0,0 +1,30 @@
+import './style.css';
+import {Link} from 'react-router-dom';
+
+export const Menu = () => {
+    return(
+        <nav className="menu">
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+
+                <li>
+                    <Link to="/about" state='This is state from About'>About</Link>
+                </li>
+
+                 <li>
+                    <Link to="/post">Posts</Link>
+                </li>
+
+                 <li>
+                    <Link to="/post/10">Post 10</Link>
+                </li>
+
+                <li>
+                    <Link to="/redirect">Redirect</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
---
+++ ProgramaTreinamento/Fase2/React-Router-v6/tutorial-react-router-dom/src/components/Post/index.tsx
@@ -0,0 +1,10 @@
+import './style.css';
+
+export const Post = () => {
+    return(
+        <div>
+            <h1>Post</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi assumenda libero omnis aliquid cumque corporis est ad repellat. Repellendus vel saepe iusto quis itaque perferendis dolorem qui cupiditate iure ipsam!</p>
+        </div>
+    );
+};
