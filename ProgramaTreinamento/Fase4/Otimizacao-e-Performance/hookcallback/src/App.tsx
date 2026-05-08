87acf2f7761ea50ab013888599e09be7ca8f5a52
Markman <davimarkmancastilho1@gmail.com>
2026-05-08 20:20:56 -0300

    dia 08/05

---
+++ ProgramaTreinamento/Fase4/Otimizacao-e-Performance/hookcallback/src/App.tsx
@@ -0,0 +1,39 @@
+import { useState, useMemo, useCallback } from 'react'
+import { Item } from './Components/item'
+
+function App() {
+  const [items, setItems] = useState<string[]>([])
+  const [wishList, setWishList] = useState<string[]>([])
+  const [newItem, setNewItem] = useState('')
+
+  function addItemToList() {
+    setItems([...items, `Item ${items.length}`]);
+  }
+
+  const addItemToWishList = useCallback((item: string) => {
+    setWishList(state => [...state, item]);
+  }, []);
+
+  const countItemsWithOne = useMemo(() => {
+    console.log('teste')
+    
+    return items.filter(item => item.includes('1')).length;
+  }, [items]);
+
+  return (
+    <div>
+      <input type="text" onChange={e => setNewItem(e.target.value)} value={newItem} />
+      <button onClick={addItemToList}>Add</button>
+      <ul>
+        {items.map(item => {
+          return <Item key={item} onAddToWishList={addItemToWishList} title={item} />
+        })}
+      </ul>
+      <div>
+        Contagem: {countItemsWithOne}
+      </div>
+    </div>
+  );
+}
+
+export default App
